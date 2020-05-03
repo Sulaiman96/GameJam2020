4eb64a4bf7156b532a99437da6c70a05b652d909
Shaquille <shaquille-etienne@hotmail.com>
2020-05-03 18:36:19 +0100

    Created a prototype of basic movement and camera functionality
    + The player looks in the direction of the mouse when right click is press
    + The camera follows the player object from a set height - Top down view
    + Added a test scene

---
+++ GameJam2020/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform player;
+    public float smooth = 0.3f;
+
+    [Tooltip("The distance between the player and camera")]
+    public float height;
+    public float cameraZOffset = 7.0f;
+    private Vector3 velocity = Vector3.zero;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = new Vector3();
+
+        position.x = player.position.x;
+        position.z = player.position.z - cameraZOffset;
+        position.y = player.position.y + height;
+        transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, smooth);
+    }
+}
---
+++ GameJam2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public GameObject camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovment();
+    }
+
+    void PlayerMovment()
+    {
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 newPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(newPosition * movementSpeed * Time.deltaTime, Space.World);
+
+        PlayerRotation(newPosition);
+    }
+
+    void PlayerRotation(Vector3 playerTranslation)
+    {
+        // When the player holds right mouse button we aim at the mouse location
+        if (Input.GetMouseButton(1))
+        {
+            Plane PlayerPlane = new Plane(Vector3.up, transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitDist = 0.0f;
+
+            if (PlayerPlane.Raycast(ray, out hitDist))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitDist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                targetRotation.x = 0;
+                targetRotation.z = 0;
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+            }
+        }
+        else
+        {
+            // Don't set the player rotation if the value is zero
+            if (playerTranslation != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerTranslation), 7f * Time.deltaTime);
+        }
+    }
+}
