9d3d76c8fcfb96a013965636b3a39ecf10a15113
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2020-05-06 23:04:41 +0100

    Two new scripts have been added. Weapon Behaviour which will be on the player and responsible for any interaction the player has with the weapon and Weapon Controller which is all the behaviour our weapon will provide us with (such as shoot).

---
+++ GameJam2020/Assets/Scripts/Weapon/WeaponBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject weaponToSpawn = null;
+    [SerializeField] private Transform weaponSpawnPosition = null;
+    [SerializeField] private Transform muzzle = null;
+    private WeaponController weapon;
+    void Start()
+    {
+        Instantiate(weaponToSpawn, weaponSpawnPosition);
+        weapon = weaponToSpawn.GetComponent<WeaponController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("HERE");
+            weapon.Shoot(muzzle);
+        }
+    }
+}
