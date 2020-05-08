49c7a0dfd51fdd8a6df26198381cd52fe0bb7dfd
Shaquille <shaquille-etienne@hotmail.com>
2020-05-08 19:45:33 +0100

    We now can fire projectiles in the cursor direction
    + projectiles bounce of objects - Bounce material
    + projectiles are constrained to the Y-axis
    + Move aiming and rotation into the weapon behaviour class

--- GameJam2020/Assets/Scripts/Player/PlayerMovement.cs
+++ GameJam2020/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
     public GameObject camera;
+    private Vector3 desiredPosition = default;
 
     // Update is called once per frame
     void Update()
@@ -18,35 +19,12 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxisRaw("Vertical");
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
 
-        Vector3 newPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(newPosition * movementSpeed * Time.deltaTime, Space.World);
-
-        PlayerRotation(newPosition);
+        desiredPosition = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.Translate(desiredPosition * movementSpeed * Time.deltaTime, Space.World);
     }
 
-    void PlayerRotation(Vector3 playerTranslation)
+    public Vector3 GetPlayerDesiredPoisition()
     {
-        // When the player holds right mouse button we aim at the mouse location
-        if (Input.GetMouseButton(1))
-        {
-            Plane PlayerPlane = new Plane(Vector3.up, transform.position);
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float hitDist = 0.0f;
-
-            if (PlayerPlane.Raycast(ray, out hitDist))
-            {
-                Vector3 targetPoint = ray.GetPoint(hitDist);
-                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-                targetRotation.x = 0;
-                targetRotation.z = 0;
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
-            }
-        }
-        else
-        {
-            // Don't set the player rotation if the value is zero
-            if (playerTranslation != Vector3.zero)
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerTranslation), 7f * Time.deltaTime);
-        }
+        return desiredPosition;
     }
 }
--- GameJam2020/Assets/Scripts/Weapon/WeaponController.cs
+++ GameJam2020/Assets/Scripts/Weapon/WeaponController.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    [SerializeField] private GameObject projectile = null;
+    [SerializeField] private GameObject projectilePrefab = null;
+    private GameObject projectile = null;
 
-    public void Shoot(Transform muzzle)
+    void Start()
     {
-        Instantiate(projectile, muzzle.position, Quaternion.identity);
+    }
+
+    public void SpawnProjectile(Transform muzzle)
+    {
+        if (projectilePrefab)
+            projectile = Instantiate(projectilePrefab, muzzle.position, Quaternion.identity) as GameObject;
+        else
+            Debug.LogWarning("No projectile prefab ");
+    }
+
+    public void Fire(Vector3 target)
+    {
+        if(projectile)
+        {
+            Vector3 dir = target - projectile.transform.position;
+            dir.y = 0;
+            projectile.GetComponent<Rigidbody>().AddForce(dir.normalized * 10f, ForceMode.Impulse);
+        }
     }
 }
