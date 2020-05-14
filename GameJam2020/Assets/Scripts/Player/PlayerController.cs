b4608e8f8b0e6f5f4fcf83c4ef1c273e9f34af5a
Shaquille <shaquille-etienne@hotmail.com>
2020-05-14 15:17:31 +0100

    Refactor player prefab to use character controller and added cursor
    + Change how the player movement works
    + We can now check if  the mouse is position behind the player and if its a valid location to aim
    + Added cursor textures
    + Fix Feedback

--- GameJam2020/Assets/Scripts/Player/PlayerController.cs
+++ GameJam2020/Assets/Scripts/Player/PlayerController.cs
@@ -7,14 +7,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     WeaponBehaviour WeaponBehaviour = null;
 
-    void Start()
+    private void Awake()
     {
         WeaponBehaviour = GetComponent<WeaponBehaviour>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
 }
