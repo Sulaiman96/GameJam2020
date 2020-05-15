d57acfae92e38d9d587825fd60d75bbf467648ae
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2020-05-15 19:06:17 +0100

    changes made

---
+++ GameJam2020/Assets/Scripts/Weapon/WeaponBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    [SerializeField] private Weapon weaponToSpawn = null;
+    [SerializeField] private Transform weaponSpawnPosition = null;
+    [SerializeField] private Transform muzzle = null;
+    private WeaponController weapon;
+    void Start()  
+    {
+        Instantiate(weaponToSpawn.weapon, weaponSpawnPosition);
+        weapon = weaponToSpawn.weapon.GetComponent<WeaponController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            weapon.Shoot(muzzle);
+            weaponToSpawn.DisplayForce();
+        }
+    }
+}
