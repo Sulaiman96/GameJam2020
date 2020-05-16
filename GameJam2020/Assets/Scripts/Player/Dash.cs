cb5f25fef743bcda7887427da8819430a9d3ff7b
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2020-05-16 23:29:15 +0100

    Added dash and removed some unwanted files

---
+++ GameJam2020/Assets/Scripts/Player/Dash.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class Dash : MonoBehaviour
+{
+    [SerializeField] float dashForce = 10f;
+    [SerializeField] private float dashWaitTime = 0.5f;
+    private CharacterController controller;
+    private Vector3 currentPosition;
+    private Vector3 previousPosition;
+    private float smoothDash = 1f;
+    private bool letPlayerDash = true;
+    private bool isDashing = false;
+    private bool dashCompleted = true;
+
+    public Vector3 GetMovementDirection
+    {
+        get { return (currentPosition - previousPosition).normalized; }
+    }
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        previousPosition = currentPosition;
+        currentPosition = transform.position;
+        if (Input.GetKeyDown(KeyCode.Space) && letPlayerDash == true)
+        {
+            letPlayerDash = false;
+            isDashing = true;
+            StartCoroutine(DashWaitTime(dashWaitTime));
+        }
+        //TODO stop the player from being able to move whilst the dash is going on.
+        if (isDashing == true ) 
+        {
+            DoTheDash();
+        }
+
+    }
+
+    private void DoTheDash()
+    {
+        controller.Move(GetMovementDirection * dashForce * Time.deltaTime);
+    }
+
+    IEnumerator DashWaitTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        letPlayerDash = true;
+        isDashing = false;
+    }
+}
