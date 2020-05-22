f657ea0cce13973158e89630005b7a08ade35d6f
Shaquille <shaquille-etienne@hotmail.com>
2020-05-22 22:54:53 +0100

    Created a health class that handles health
    + We broadcast health changes to any listeners
    + Health comp can check if the damage taken was from the same team

---
+++ GameJam2020/Assets/Scripts/Player/HealthController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    
+    [SerializeField] private float maxHealth = 10f;
+
+    public byte TeamNumber = 0;  
+
+    private float health = 10f;
+    private bool bIsDead = false;
+
+    public event EventHandler<OnHealthChangeEventArgs> OnHealthChange;
+    public class OnHealthChangeEventArgs : EventArgs
+    {
+        public float damageAmount;
+        public float currentHealth;
+        public GameObject owner;
+        public GameObject instigator;
+    }
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void OnTakeDamage(float damage, GameObject _instigator)
+    {
+        HealthController instigatorHealthController;
+        if (_instigator.TryGetComponent<HealthController>(out instigatorHealthController))
+        {
+            if (instigatorHealthController.TeamNumber == TeamNumber)
+                return;
+        }
+
+        health = health - damage;
+        
+        if(health <= 0)
+        {
+            bIsDead = true;
+        }
+
+        OnHealthChange?.Invoke(this, new OnHealthChangeEventArgs { damageAmount = damage, currentHealth = health, owner = gameObject, instigator = _instigator });
+    }
+}
