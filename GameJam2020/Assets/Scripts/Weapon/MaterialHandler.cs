e348418512014124ce55bedee7d5150c0b4119b8
Shaquille <shaquille-etienne@hotmail.com>
2020-05-25 20:09:29 +0100

    Added material to the player projectile prefab and spawn particle when projectile is destroyed
    + Refactor behaviour when projectile is destroyed
    + Create a material handler to change material when needed

---
+++ GameJam2020/Assets/Scripts/Weapon/MaterialHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialHandler : MonoBehaviour
+{
+    public Material defaultMaterial;
+    public Material activeMaterial;
+    public bool isActiveMaterial { get; set; } = false;
+
+    private MeshRenderer meshRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRender = GetComponent<MeshRenderer>();
+        if (defaultMaterial)
+        {
+            meshRender.material = defaultMaterial;
+        }
+        else
+        {
+            // If we don't have default material we store current material
+            defaultMaterial = meshRender.material;
+        }
+    }
+
+    public void UseActiveMaterial()
+    {
+        if (!activeMaterial)
+            return;
+
+        meshRender.material = activeMaterial;
+        isActiveMaterial = true;
+    }
+
+    public void UseDefaultMaterial()
+    {
+        if (!defaultMaterial)
+            return;
+
+        meshRender.material = defaultMaterial;
+        isActiveMaterial = false;
+    }
+
+    public void ChangeActiveMaterial(Material mat)
+    {
+        activeMaterial = mat;
+    }
+}
--- GameJam2020/Assets/Scripts/Weapon/ProjectileBehaviour.cs
+++ GameJam2020/Assets/Scripts/Weapon/ProjectileBehaviour.cs
@@ -15,13 +15,29 @@ public class ProjectileBehaviour : MonoBehaviour
     private TimeController timeController;
     private float timeDelation = 1;
     private int currentBounce = 0;
+    private MaterialHandler materialHandler;
+    private float currentLifeSpan;
 
+    public ParticleSystem destroyParticle;
     public EventHandler OnProjectileDestroyed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         timeController = GetComponent<TimeController>();
+        materialHandler = GetComponent<MaterialHandler>();
+
+        currentLifeSpan = lifeSpan;
+    }
+
+    private void Update()
+    {
+        currentLifeSpan -= Time.deltaTime;
+        if (currentLifeSpan < 0 && bIsDestroyingProjectile == false)
+        {
+            bIsDestroyingProjectile = true;
+            DestroyProjectile();
+        }
     }
 
     private void Start()
@@ -43,28 +59,36 @@ public class ProjectileBehaviour : MonoBehaviour
 
     public void LaunchProjectile(Vector3 targetLocation)
     {
+        if(materialHandler && !materialHandler.isActiveMaterial)
+            materialHandler.UseActiveMaterial();
 
         Vector3 directionToLaunch = (targetLocation - transform.position).normalized;
         directionToLaunch.y = 0;
         rb.velocity = Vector3.zero;
         rb.AddForce(directionToLaunch * projectileSpeed * timeDelation, ForceMode.Impulse);
 
-        if (bIsDestroyingProjectile == true)
-            return;
-
-        Destroy(gameObject, lifeSpan);
-        bIsDestroyingProjectile = true;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (++currentBounce >= totalBounceAmount)
-            Destroy(gameObject);
+            DestroyProjectile();
     }
 
     private void OnDestroy()
     {
         OnProjectileDestroyed?.Invoke(this.gameObject, EventArgs.Empty);
+    }
+
+    private void DestroyProjectile()
+    {
+        if (destroyParticle)
+        {
+            var spawnedImpact = Instantiate(destroyParticle.gameObject, transform.position, transform.rotation) as GameObject;
+            Destroy(spawnedImpact, destroyParticle.main.duration);
+        }
+        
+        Destroy(gameObject);
     }
 
 }
