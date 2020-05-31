3b4b51899b8dfd23545bdbad6d53d17e1d1cafc1
Shaquille <shaquille-etienne@hotmail.com>
2020-05-31 18:40:36 +0100

    Created a kill volume to destroy all objects that enter it

---
+++ GameJam2020/Assets/Scripts/KillVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillVolume : MonoBehaviour
+{
+     private const float maxDamage = 9999f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // destroy everything that enters it but
+
+        HealthController hc;
+        if(other.TryGetComponent<HealthController>(out hc))
+        {
+            hc.OnTakeDamage(maxDamage, gameObject);
+        }
+
+        Destroy(other.transform.root.gameObject);
+    }
+}
